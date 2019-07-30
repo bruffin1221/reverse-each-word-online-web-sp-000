d9ff83c8580383b5ee808db6dd9a7e4bb730ce55
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-30 04:38:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,6 @@ def reverse_each_word(sentence)
   reverse=[]
   sentence.split.each do |word|
     reverse<<"#{word.reverse}"
-  sentence.split.collect do |word|
-    "#{word.reverse}"
   end
   reverse.join
 end
